0588b54ef0548dfce915cd49a3c2e2e55d135dff
metanivek <metanivek@gmail.com>
2023-06-30 15:48:12 -0400

    eio: add lwt comparison

--- eio/README.md
+++ eio/README.md
@@ -1,9 +1,13 @@
 ``` sh
 # dune exec ./main.exe -- ./test.dat
 Unix.read
-0.0383ms        0.0008ms
+0.0461ms        0.0009ms
 Index.pread
-0.0173ms        0.0003ms
+0.0221ms        0.0004ms
 Eio.File.pread
-0.0591ms        0.0012ms
+0.0681ms        0.0014ms
+Eio.File.pread (with polling)
+0.2318ms        0.0046ms
+Lwt_unix.pread
+0.6481ms        0.0130ms
 ```
--- eio/main.ml
+++ eio/main.ml
@@ -20,6 +20,24 @@ let measure_read size_range chunk_num max_offset f =
   Fmt.epr "%.4fms\t%.4fms@." duration (duration /. Float.of_int chunk_num);
   ()
 
+let measure_read_lwt size_range chunk_num max_offset f =
+  let open Lwt.Syntax in
+  let lower, upper = size_range in
+  let range = upper - lower in
+  let c0 = Mtime_clock.counter () in
+  let* () =
+    List.init chunk_num Fun.id
+    |> Lwt_list.iter_s (fun _ ->
+           let offset = Random.full_int max_offset in
+           let len = lower + Random.full_int range in
+           let* read_len = f offset len in
+           assert (len = read_len);
+           Lwt.return_unit)
+  in
+  let duration = Mtime_clock.count c0 |> Mtime.span_to_ms in
+  Fmt.epr "%.4fms\t%.4fms@." duration (duration /. Float.of_int chunk_num);
+  Lwt.return_unit
+
 let () =
   let path = Sys.argv.(1) in
   let file = Unix.openfile path [ Unix.O_RDONLY ] 0 in
@@ -27,6 +45,7 @@ let () =
   let range = (50, 100) in
   let chunk_num = 50 in
   let measure = measure_read range chunk_num (len - snd range) in
+  let measure_lwt = measure_read_lwt range chunk_num (len - snd range) in
 
   Fmt.epr "Unix.read@.";
   let () =
@@ -56,4 +75,23 @@ let () =
     Eio.File.pread flow ~file_offset:(Optint.Int63.of_int pos) [ buf ]
   in
 
-  ()
+  Fmt.epr "Eio.File.pread (with polling)@.";
+  Eio_linux.run ~polling_timeout:5000 @@ fun env ->
+  Eio.Switch.run @@ fun sw ->
+  let flow = Eio.Path.open_in ~sw Eio.Path.(env#fs / path) in
+  let () =
+    measure @@ fun pos len ->
+    let buf = Cstruct.create len in
+    Eio.File.pread flow ~file_offset:(Optint.Int63.of_int pos) [ buf ]
+  in
+
+  Fmt.epr "Lwt_unix.pread@.";
+  Lwt_main.run
+    (let open Lwt.Syntax in
+     let* file = Lwt_unix.openfile path [ Unix.O_RDONLY ] 0 in
+     let* () =
+       measure_lwt @@ fun pos len ->
+       let buffer = Bytes.create len in
+       Lwt_unix.pread file buffer ~file_offset:pos 0 len
+     in
+     Lwt.return_unit)
